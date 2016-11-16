fb6be190a533bd8d246b598db43d13b88629459c
Haochen <haochen_233333@163.com>
2016-11-16 10:04:22 +0800

    add a JQuery test

---
+++ asp.NET/TestJQuery/TestJQuery/Controllers/TestJQueryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestJQuery.Controllers
+{
+    public class TestJQueryController : Controller
+    {
+        //
+        // GET: /TestJQuery/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ asp.NET/TestJQuery/TestJQuery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestJQuery.MvcApplication" Language="C#" %>
