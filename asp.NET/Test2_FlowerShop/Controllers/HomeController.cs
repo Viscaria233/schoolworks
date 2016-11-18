b63557b9fa577490d4925ea3d7df8cc730a991dc
Haochen <haochen_233333@163.com>
2016-11-18 22:44:38 +0800

    finish the asp.net test2

---
+++ asp.NET/Test2_FlowerShop/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test2_FlowerShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Category()
+        {
+            return View();
+        }
+
+        public ActionResult Specials()
+        {
+            return View();
+        }
+
+        public ActionResult MyAccount()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            return View();
+        }
+
+    }
+}
